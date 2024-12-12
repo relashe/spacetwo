1a1dfbcdfda14ad5718c5cf48e29904a4712ae81
Paciência Canda <paciencia@relashe.com>
2024-12-12 15:30:58 +0000

    - handle successful login/signup on login page

--- src/components/LoginActions/GoogleSignUp.tsx
+++ src/components/LoginActions/GoogleSignUp.tsx
@@ -1,31 +1,63 @@
-import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import { useAuthContext } from "../../providers";
 import { useNavigate } from "react-router";
 import { APP_ROUTES } from "../../routing";
+import useSpaceTwoUsers from "../../hooks/useSpaceTwoUsers";
+import { apiService } from "../..";
+import { API_ENDPOINTS } from "../../constants";
+import { Button } from "@mui/material";
+import { GoogleProfile } from "../../types/google.types";
 
 const GoogleSignUp: React.FC<{}> = ({}) => {
   /* hooks */
-  const { setToken } = useAuthContext();
+  const { setToken, setUser } = useAuthContext();
   const navigate = useNavigate();
+  const { getSpaceTwoUserWithEmail } = useSpaceTwoUsers();
 
   /* events */
-  const handleSuccessLogin = (credentialResponse: CredentialResponse) => {
-    console.log(credentialResponse);
+  const handleGoogleLogin = useGoogleLogin({
+    onSuccess: async (token) => {
+      setToken(token.access_token);
 
-    if (credentialResponse.credential) {
-      setToken(credentialResponse.credential);
-      navigate(APP_ROUTES.HOME);
-    }
-  };
+      // get Google Info
+      const userProfile = await apiService.get<GoogleProfile>(
+        API_ENDPOINTS.GOOGLE.GET_USER_INFO(token.access_token),
+        token.access_token
+      );
+
+      // check if user exists
+      if (!userProfile) {
+        navigate(APP_ROUTES.CREATE_ACCOUNT);
+      } else {
+        const spaceTwoUser = await getSpaceTwoUserWithEmail(userProfile.email);
+
+        if (!spaceTwoUser) {
+          navigate(
+            `${APP_ROUTES.CREATE_ACCOUNT}?email=${userProfile.email}&name=${userProfile.given_name}%20${userProfile.family_name}`
+          );
+        } else {
+          setUser(spaceTwoUser);
+          navigate(APP_ROUTES.HOME);
+        }
+      }
+
+      console.info(userProfile);
+    },
+    onError: () => {
+      console.log("Login Failed");
+    },
+  });
 
   return (
-    <GoogleLogin
-      auto_select
-      onSuccess={handleSuccessLogin}
-      onError={() => {
-        console.log("Login Failed");
-      }}
-    />
+    <>
+      <Button
+        onClick={() => {
+          handleGoogleLogin();
+        }}
+      >
+        Log In
+      </Button>
+    </>
   );
 };
 
