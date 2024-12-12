ff72a5797aa5730ed26a08cb402ad4a8ad6d3ed8
Paciência Canda <paciencia@relashe.com>
2024-12-12 15:29:45 +0000

    -main file

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+// This is the index file that is built for the driver's app
+
+export * from "./components";
+
+// TYPES
+export * from "./types";
+
+// CONSTANTS
+export * from "./constants";
+
+// HOOKS
+export * from "./hooks";
+
+// DB
+export * from "./db";
+
+// HELPERS
+export * from "./helpers";
