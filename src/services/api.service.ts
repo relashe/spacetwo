2deec09f0f62e815f372a8c7eb8d7891e18b94cd
Paciência Canda <paciencia@relashe.com>
2024-12-13 00:06:24 +0000

    - unused imports removed

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,8 +1,6 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios from "axios";
 
 export class ApiService {
-  constructor() {}
-
   async get<TResponse>(
     endpoint: string,
     accessToken: string
